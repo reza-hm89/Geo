42abe693981f53662029350cbd162cd58305176f
reza-hm89 <reza_hm2008@yahoo.com>
2019-12-23 01:11:46 +0330

    Set to choose in memory or sql db

--- Services/Geographical/DistanceService.cs
+++ Services/Geographical/DistanceService.cs
@@ -1,9 +1,11 @@
 ﻿using DAL;
+using Microsoft.EntityFrameworkCore;
 using Model.Geographical;
 using ServiceContract.Geographical;
 using ServiceStack.Redis;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -37,21 +39,30 @@ namespace Service.Geographical
 
         public IEnumerable<object> GetByUserId(string userId, bool useInMemory = true)
         {
-            var distances = FindAll(x => x.UserId == userId);
-
             if (useInMemory)
             {
+                var distances1 = _context.UserDistances
+                                  .AsNoTracking()
+                                  .Where(x => x.UserId == userId)
+                                  .ToList();
+
                 RedisClient client = new RedisClient();
 
                 var list = new List<object>();
 
-                foreach (var item in distances)
+                foreach (var item in distances1)
                 {
                     list.Add(client.Get<double>(item.UserId + item.Id));
                 }
 
                 return list;
             }
+
+            var distances = _context.UserDistances
+                                   .AsNoTracking()
+                                   .Where(x => x.UserId == userId)
+                                   .Select(x => (object)x.Distance)
+                                   .ToList();
 
             return distances;
         }
